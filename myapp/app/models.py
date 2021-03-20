aa9aa07ff3982fa6fe00e5462b92b7651c1e3844
Jean Lionel <nijeanlionel@gmail.com>
2021-03-20 17:31:10 +0200

    Commande Model

--- myapp/app/models.py
+++ myapp/app/models.py
@@ -2,13 +2,34 @@ from django.db import models
 
 # Create your models here.
 
+class Commande(models.Model):
+	groupes_sanguis_list = (
+		("A+","A+"),
+		("B+","B+"),
+		("AB+","AB+"),
+		("O+","O+"),
+		("A-","A-"),
+		("B-","B-"),
+		("AB-","AB-"),
+		("O-","O-"),
+		)
+ 	 	 			
+	services=(
+		("Pédiatrie","Pédiatrie"),
+		("Maternité","Maternité"),
+		("Médecine interne","Médecine interne"),
+		("Bloc Opératoire","Bloc Opératoire"),
+		("Néonatologie","Néonatologie"),
+		("Autres services","Autres services"),
+		("Quantité détruite","Quantité détruite"),
+		)
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+	groupe_sanguin = models.CharField(max_length=25,choices=groupes_sanguis_list)
+	date = models.DateField()
+	quantite_recu = models.FloatField()
+	service = models.CharField(max_length=50, choices=services)
+	quantite_restant = models.FloatField()
+	quantite_demandee = models.FloatField()
+	quantite_servi = models.FloatField()
+	observations = models.TextField()
 
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
